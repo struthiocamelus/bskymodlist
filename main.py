94ab10fe95b134464f4d8037103cd079dd550569
Ostrich <ostrich.semen@gmail.com>
2024-05-19 16:06:23 -0400

    add find-all-lists command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 from datetime import datetime, timezone
 import json
 
@@ -28,11 +29,12 @@ def search_actors(query, limit, cursor=None):
 def drain_all_actors(query):
     limit = 100
     response = search_actors(query, limit)
+    for actor in response.actors:
+        yield actor
     while response.cursor is not None:
+        response = search_actors(query, limit, response.cursor)
         for actor in response.actors:
             yield actor
-        cursor = response.cursor
-        response = search_actors(query, limit, cursor)
 
 
 def search_user(username):
@@ -85,13 +87,66 @@ def create_atproto_list_item(list_uri, did, my_did):
     })
 
 
-def add_to_moderation_list(list_name, filename):
+@backoff.on_exception(
+    backoff.expo,
+    atproto_client.exceptions.RequestException,
+    max_time=300
+)
+def get_atproto_list(list_key, my_did):
+    return client.app.bsky.graph.get_list({
+        "list": f"at://{my_did}/app.bsky.graph.list/{list_key}",
+        "limit": 1
+    })
+
+
+@backoff.on_exception(
+    backoff.expo,
+    atproto_client.exceptions.RequestException,
+    max_time=300
+)
+def get_atproto_lists(did, limit=100, cursor=None):
+    return client.app.bsky.graph.get_lists({
+        "actor": did,
+        "limit": limit,
+        "cursor": cursor
+    })
+
+
+def drain_atproto_lists(did):
+    response = get_atproto_lists(did)
+    for mod_list in response.lists:
+        yield mod_list
+    while response.cursor is not None:
+        response = get_atproto_lists(did, cursor=response.cursor)
+        for mod_list in response.lists:
+            yield mod_list
+
+
+def find_all_lists():
+    """Find all moderation lists for user."""
+    try:
+        my_did = atproto_client_info['login_response']['did']
+        lists = drain_atproto_lists(my_did)
+        for mod_list in lists:
+            print(f"{mod_list.uri} -- {mod_list.name} {mod_list.description}")
+    except Exception as e:
+        click.echo(f"An error occurred: {e}")
+
+
+def add_to_moderation_list(list_name=None, list_did=None, filename=None):
     """Add users from a newline delimited file to the moderation list."""
     try:
         my_did = atproto_client_info['login_response']['did']
         with open(filename, 'r') as file:
             lines = file.read().splitlines()
-        new_modlist = create_atproto_list(list_name, my_did)
+
+        if list_name is not None:
+            moderation_list = create_atproto_list(list_name, my_did)
+        elif list_did is not None:
+            moderation_list = get_atproto_list(list_did, my_did)
+            moderation_list = moderation_list['list']
+        else:
+            raise RuntimeError("This should never happen")
 
         for line in lines:
             did = extract_did(line)
@@ -99,7 +154,7 @@ def add_to_moderation_list(list_name, filename):
                 click.echo(f"{did} doesn't look like a DID, skipping...")
             click.echo(f"Adding record to modlist: {line}")
             try:
-                create_atproto_list_item(new_modlist['uri'], did, my_did)
+                create_atproto_list_item(moderation_list['uri'], did, my_did)
             except Exception as e:
                 click.echo(f"Failed to add {did} to modlist, continuing...")
 
@@ -141,11 +196,20 @@ def search(username):
 
 
 @cli.command()
+def find_lists():
+    find_all_lists()
+
+
+@cli.command()
 @click.option("--list-name", help="Name of the list")
+@click.option("--list-key", help="Key of existing list. Use find-lists to find your lists.")
 @click.argument('filename', type=click.Path(exists=True))
-def add(list_name, filename):
+def add(list_name, list_did, filename):
+    if list_name is None and list_did is None or list_name is not None and list_did is not None:
+        click.echo("Only one of --list-name or --list-did is required. Use --help for help.")
+        sys.exit(1)
     """Add users from a newline delimited file to the moderation list."""
-    add_to_moderation_list(list_name, filename)
+    add_to_moderation_list(list_name, list_did, filename)
 
 
 if __name__ == '__main__':
